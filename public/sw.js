c59acdfbe81f9fe2c19f38a0e0f859d523370f15
VOIDZ-ux <firemws@gmail.com>
2026-05-27 23:15:46 +0200

    bump SW to v2, skip waiting, claim clients

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taskomon-v1';
+const CACHE_NAME = 'taskomon-v2';
 const ASSETS = [
   '/Taskomon/',
   '/Taskomon/index.html',
@@ -20,6 +20,16 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
