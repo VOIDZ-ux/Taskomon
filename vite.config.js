80cc6562f762bf6e2b1cab9b17ef002d29601844
VOIDZ-ux <firemws@gmail.com>
2026-05-27 22:58:31 +0200

    fix base path case sensitivity

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/taskomon/',
+  base: '/Taskomon/',
   server: {
     port: 5173,
     host: true,
